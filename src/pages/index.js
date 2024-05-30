6a44c7a8ee1d50384ee7252627ccf6541ec46219
Asad Bukhari <asad@plastk.ca>
2024-05-30 15:11:41 +0500

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,19 @@
-import React, { useEffect } from "react";
-import { useRouter } from "next/router";
-import { getCookie } from "@/helpers/common";
+import React, {useEffect} from "react";
+import {useRouter} from "next/router";
+import {getCookie} from "@/helpers/common";
 
 const Home = () => {
-  const allowedPages = getCookie(process.env.NEXT_PUBLIC_ALLOWED_PAGES_COOKIE);
-  const router = useRouter();
+    const allowedPages = getCookie(process.env.NEXT_PUBLIC_ALLOWED_PAGES_COOKIE);
+    const router = useRouter();
 
-  useEffect(() => {
-    if (allowedPages) {
-      router.push(allowedPages[0]);
-    } else {
-      router.push("/sign-in");
-    }
-  });
-  return <div></div>;
+    useEffect(() => {
+        if (allowedPages) {
+            router.push(allowedPages[0]);
+        } else {
+            router.push("/sign-in");
+        }
+    });
+    return <div></div>;
 };
 
 export default Home;
