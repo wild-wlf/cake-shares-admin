5f5dd291c125549d676ab39432457ac8d1bf2e1a
Asad Bukhari <asad@plastk.ca>
2024-06-13 20:57:19 +0500

    up

--- src/components/atoms/PermissionsTable/index.jsx
+++ src/components/atoms/PermissionsTable/index.jsx
@@ -1,19 +1,19 @@
-import React, { useMemo, useState } from "react";
-import { ActionBtnList } from "@/components/atoms/ActionBtns/ActionBtns.styles";
-import Table from "@/components/molecules/Table";
-import TableLayout from "@/components/atoms/TableLayout";
-import CenterModal from "@/components/molecules/Modal/CenterModal";
-import SuccessIcon from "../../../_assets/successIcon.png";
-import { MdModeEditOutline } from "react-icons/md";
-import DeleteIcon from "../../../_assets/delete.svg";
-import TableStyle from "../../../_assets/table-style.jpg";
-import InfoIcon from "../../../_assets/infoIcon.png";
-import Image from "next/image";
-import { TableContainer } from "./PermissionsTable.style";
-import DeletePermissionModal from "./DeletePermissionModal";
-import SuccessfulModal from "../ProductDeleteModal/SuccessfulModal";
-import CreatePermissionModal from "./CreatePermissionModal";
-import EditPermissionModal from "./EditPermissionModal";
+import React, { useMemo, useState } from 'react';
+import { ActionBtnList } from '@/components/atoms/ActionBtns/ActionBtns.styles';
+import Table from '@/components/molecules/Table';
+import TableLayout from '@/components/atoms/TableLayout';
+import CenterModal from '@/components/molecules/Modal/CenterModal';
+import SuccessIcon from '../../../_assets/successIcon.png';
+import { MdModeEditOutline } from 'react-icons/md';
+import DeleteIcon from '../../../_assets/delete.svg';
+import TableStyle from '../../../_assets/table-style.jpg';
+import InfoIcon from '../../../_assets/infoIcon.png';
+import Image from 'next/image';
+import { TableContainer } from './PermissionsTable.style';
+import DeletePermissionModal from './DeletePermissionModal';
+import SuccessfulModal from '../ProductDeleteModal/SuccessfulModal';
+import CreatePermissionModal from './CreatePermissionModal';
+import EditPermissionModal from './EditPermissionModal';
 
 const PermissionTable = ({ title }) => {
   const [deleteModal, setDeleteModal] = useState(false);
@@ -24,69 +24,61 @@ const PermissionTable = ({ title }) => {
 
   const permissions = [
     {
-      created_at: "27/04/2024",
-      can_do: "permission.delete",
-      desc: "can delete permission",
+      created_at: '27/04/2024',
+      can_do: 'permission.delete',
+      desc: 'can delete permission',
     },
     {
-      created_at: "27/04/2024",
-      can_do: "permission.edit",
-      desc: "can edit details of permission",
+      created_at: '27/04/2024',
+      can_do: 'permission.edit',
+      desc: 'can edit details of permission',
     },
     {
-      created_at: "27/04/2024",
-      can_do: "permission.delete",
-      desc: "can delete permission",
+      created_at: '27/04/2024',
+      can_do: 'permission.delete',
+      desc: 'can delete permission',
     },
     {
-      created_at: "27/04/2024",
-      can_do: "permission.edit",
-      desc: "can edit details of permission",
+      created_at: '27/04/2024',
+      can_do: 'permission.edit',
+      desc: 'can edit details of permission',
     },
     {
-      created_at: "27/04/2024",
-      can_do: "permission.delete",
-      desc: "can delete permission",
+      created_at: '27/04/2024',
+      can_do: 'permission.delete',
+      desc: 'can delete permission',
     },
     {
-      created_at: "27/04/2024",
-      can_do: "permission.edit",
-      desc: "can edit details of permission",
+      created_at: '27/04/2024',
+      can_do: 'permission.edit',
+      desc: 'can edit details of permission',
     },
     {
-      created_at: "27/04/2024",
-      can_do: "permission.delete",
-      desc: "can delete permission",
+      created_at: '27/04/2024',
+      can_do: 'permission.delete',
+      desc: 'can delete permission',
     },
     {
-      created_at: "27/04/2024",
-      can_do: "permission.edit",
-      desc: "can edit details of permission",
+      created_at: '27/04/2024',
+      can_do: 'permission.edit',
+      desc: 'can edit details of permission',
     },
   ];
 
-  const handleEditModal = (e) => {
+  const handleEditModal = e => {
     setEditPermissionModal(true);
     setPermissionToUpdate(e);
   };
-  const actionBtns = (_) => (
+  const actionBtns = _ => (
     <>
       <ActionBtnList>
         <li>
-          <button
-            type="button"
-            className="btn edit"
-            onClick={() => handleEditModal(_)}
-          >
+          <button type="button" className="btn edit" onClick={() => handleEditModal(_)}>
             <MdModeEditOutline color="rgba(64, 143, 140, 1)" size={16} />
           </button>
         </li>
         <li>
-          <button
-            type="button"
-            className="btn delete"
-            onClick={() => setDeleteModal(true)}
-          >
+          <button type="button" className="btn delete" onClick={() => setDeleteModal(true)}>
             <Image src={DeleteIcon} alt="delete" />
           </button>
         </li>
@@ -95,22 +87,21 @@ const PermissionTable = ({ title }) => {
   );
 
   const { product_rows, totalItems } = useMemo(() => ({
-    product_rows: permissions?.map((permission) => [
-      permission.created_at || "------------",
-      permission.can_do || "------------",
-      permission.desc || "------------",
+    product_rows: permissions?.map(permission => [
+      permission.created_at || '------------',
+      permission.can_do || '------------',
+      permission.desc || '------------',
       actionBtns(permission),
     ]),
   }));
-  const columnNamess = [`Created at`, `Can Do`, `Description`, "Actions"];
+  const columnNamess = [`Created at`, `Can Do`, `Description`, 'Actions'];
   return (
     <>
       <CenterModal
         open={deleteModal}
         setOpen={setDeleteModal}
         title={<Image src={InfoIcon} alt="InfoIcon" />}
-        width="543"
-      >
+        width="543">
         <DeletePermissionModal
           closeDeleteModal={() => {
             setDeleteModal(false);
@@ -124,17 +115,15 @@ const PermissionTable = ({ title }) => {
         open={deleteSuccesfulModal}
         setOpen={setDeleteSuccessfulModal}
         title={<Image src={SuccessIcon} alt="SuccessIcon" />}
-        width="543"
-      >
-        <SuccessfulModal title={"Permission Deleted Successfully!"} />
+        width="543">
+        <SuccessfulModal title={'Permission Deleted Successfully!'} />
       </CenterModal>
 
       <CenterModal
         open={createPermissionModal}
         setOpen={setCreatePermissionModal}
-        title={"Create Permission"}
-        width="669"
-      >
+        title={'Create Permission'}
+        width="669">
         <CreatePermissionModal
           closeModal={() => {
             setCreatePermissionModal(false);
@@ -142,12 +131,7 @@ const PermissionTable = ({ title }) => {
         />
       </CenterModal>
 
-      <CenterModal
-        open={editPermissionModal}
-        setOpen={setEditPermissionModal}
-        title={"Edit Permission"}
-        width="669"
-      >
+      <CenterModal open={editPermissionModal} setOpen={setEditPermissionModal} title={'Edit Permission'} width="669">
         <EditPermissionModal
           closeModal={() => {
             setEditPermissionModal(false);
@@ -159,7 +143,7 @@ const PermissionTable = ({ title }) => {
       <TableContainer>
         <Image src={TableStyle} className="tableStyle" alt="tableStyle" />
         <TableLayout
-          tableHeading={" "}
+          tableHeading={' '}
           placeholder="Search Permission"
           btnType="blue"
           btnText="+ Create Permission"
@@ -167,8 +151,7 @@ const PermissionTable = ({ title }) => {
           openModal={() => {
             setCreatePermissionModal(true);
           }}
-          filterBlock={true}
-        >
+          filterBlock={true}>
           <Table
             width={1024}
             rowsData={product_rows}
